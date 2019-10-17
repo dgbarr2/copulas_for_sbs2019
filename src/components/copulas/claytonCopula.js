28906ff9119c17a049c0e5db8d6c8e1fabc15216
David <dgbarr@mac.com>
2019-10-17 15:43:23 +0100

    Corrected errors in 3var Clayton components.

--- src/components/copulas/claytonCopula.js
+++ src/components/copulas/claytonCopula.js
@@ -2,7 +2,7 @@ import React from "react"
 
 
 const claytonCopula = (u,w,θ) => {
-    const v = ( (w**(-θ/(θ+1)) + 1)*u**(-θ) -1)**(-1/θ)
+    const v = ( (w**(-θ/(θ+1)) - 1)*u**(-θ) +1)**(-1/θ)
     return v
 }
 export default claytonCopula
